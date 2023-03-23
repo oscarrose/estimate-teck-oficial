4f36f04530bfcf6c3906a218998a0a27f5271283
Rosario Reyes <rr2018-0340@uce.edu.do>
2023-03-23 17:54:56 -0400

    Fix backend de TarifarioHora

--- estimate-teck/Models/TarifarioHora.cs
+++ estimate-teck/Models/TarifarioHora.cs
@@ -8,21 +8,18 @@ namespace estimate_teck.Models;
 public partial class TarifarioHora
 {
 
-    public partial class TarifarioHora
-    {
-        public int TarifarioId { get; set; }
-        public int CargoId { get; set; }
-        public int UsuarioId { get; set; }
-        public decimal MontoTarifa { get; set; }
-        public DateTime FechaCreacion { get; set; }
+    public int TarifarioId { get; set; }
+    public int CargoId { get; set; }
+    public int UsuarioId { get; set; }
+    public decimal MontoTarifa { get; set; }
+    public DateTime FechaCreacion { get; set; }
 
-        [JsonIgnore]
-        [IgnoreDataMember]
-        public virtual Cargo? Cargo { get; set; } = null!;
-        [JsonPropertyName("Usuario")]
-        [JsonIgnore]
-        [IgnoreDataMember]
-        public virtual Usuario? Usuario { get; set; } = null!;
-    }
+    [JsonIgnore]
+    [IgnoreDataMember]
+    public virtual Cargo? Cargo { get; set; } = null!;
+    [JsonPropertyName("Usuario")]
+    [JsonIgnore]
+    [IgnoreDataMember]
+    public virtual Usuario? Usuario { get; set; } = null!;
 
 }
